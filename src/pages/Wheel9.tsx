bc63b461c1950c0dec77edefbf773feaff6e8439
anas12312 <anashesham000@gmail.com>
2024-04-10 20:13:23 +0200

    anas

--- src/pages/Wheel9.tsx
+++ src/pages/Wheel9.tsx
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import WheelComponent from '../components/WheelComponent';
-import Test from '../components/Test';
 
 export default function Wheel9() {
-    const [winner, setWinner] = useState("Spin")
+    const [active, setActive] = useState(true)
     const primaryColor = "black"
     const contrastColor = "white"
-    const upDuration = 3000
-    const downDuration = 3000
+    const upDuration = 100
+    const downDuration = 500
     const segments: [string, string, string, string, string, string, string, string, string] = [
         "red",
         "light yellow",
@@ -31,33 +30,41 @@ export default function Wheel9() {
         "#6F2200"
     ];
     const onFinished = (winner: string) => {
-        console.log(new Date())
-        setWinner(winner)
+        setActive(true)
+        console.log(winner)
     };
     useEffect(() => {
         document.getElementById('label')!.style.backgroundColor = primaryColor;
         document.getElementById('label')!.style.color = contrastColor
     }, [])
     return (
-        <div className='relative' onClick={() => { console.log(new Date()) }}>
-            <div id='label' className='pointer-events-none absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-12 h-12 bg-black text-white flex justify-center text-center items-center font-bold'>
-                {winner}
+        <div className='relative w-full h-full'>
+            <div className='flex items-center top-5'>
+                <input className='w-5 h-5 mr-2' type="checkbox" id="demoCheckbox" name="checkbox" checked={active} onChange={(e) => { setActive(e.target.checked) }} />
+                <label htmlFor="demoCheckbox">Enable</label>
             </div>
-            <WheelComponent
-                setWinner={setWinner}
-                winningSegment=''
-                segments={segments}
-                segColors={segColors}
-                // winningSegment='won 10'
-                onFinished={(winner) => onFinished(winner)}
-                primaryColor={primaryColor}
-                contrastColor={contrastColor}
-                buttonText="Spin"
-                isOnlyOnce={false}
-                size={200}
-                upDuration={upDuration}
-                downDuration={downDuration}
-                fontFamily="Arial" />
+
+            <div className='relative'>
+                {!active && (
+                    <div className='absolute w-full h-full'></div>
+                )}
+                <div onClick={() => setActive(false)}>
+                    <WheelComponent
+                        winningSegment=''
+                        segments={segments}
+                        segColors={segColors}
+                        // winningSegment='won 10'
+                        onFinished={(winner) => onFinished(winner)}
+                        primaryColor={primaryColor}
+                        contrastColor={contrastColor}
+                        buttonText="Spin"
+                        isOnlyOnce={false}
+                        size={200}
+                        upDuration={upDuration}
+                        downDuration={downDuration}
+                        fontFamily="Arial" />
+                </div>
+            </div>
         </div>
     )
 }
